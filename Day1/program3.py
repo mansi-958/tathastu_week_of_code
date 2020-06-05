056aaa0fe8cfec4a4051f6032993235f44c897fe
mansi-958 <59793804+mansi-958@users.noreply.github.com>
2020-06-05 02:06:21 -0700

    Create program3.py

---
+++ Day1/program3.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+num2=num1+num2
+num1=num2-num1
+num2=num2-num1
+print("After swap")
+print("First number is:",num1)
+
+print("Second number is:",num2)
+
